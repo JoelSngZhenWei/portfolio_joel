73d72538e7d7aa6353d5256951af71b0edaf3ff8
JoelSngZhenWei <joelsngzw@gmail.com>
2025-01-20 04:13:41 +0800

    testing vercel deployment

--- portfolio_joel_1/app/projects/page.tsx
+++ portfolio_joel_1/app/projects/page.tsx
@@ -51,7 +51,7 @@ export default function Projects() {
                 {/* Top */}
                 <div className="w-full flex justify-between items-center">
                   <div className="text-3xl xl:text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">{project.num}</div>
-                  <Link href={project.href} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45">
+                  <Link href={project.href} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center group-hover:-rotate-45">
                     <BsArrowDownRight className="text-primary text-3xl" />
                   </Link>
                 </div>
