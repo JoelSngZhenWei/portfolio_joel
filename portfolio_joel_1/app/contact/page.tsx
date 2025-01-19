ac62b3f67f0f4b57973b0aae878790fca198ccb9
JoelSngZhenWei <joelsngzw@gmail.com>
2025-01-20 00:29:58 +0800

    testing vercel deployment

--- portfolio_joel_1/app/contact/page.tsx
+++ portfolio_joel_1/app/contact/page.tsx
@@ -3,9 +3,8 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
 
-import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa'
+import { FaPhoneAlt, FaEnvelope } from 'react-icons/fa'
 
 const info = [
   {
@@ -17,12 +16,7 @@ const info = [
     icon: <FaEnvelope />,
     title: 'Email',
     description: 'joelsngzw@gmail.com'
-  },
-  {
-    icon: <FaMapMarkerAlt />,
-    title: 'Address',
-    description: 'Singapore, 556185'
-  },
+  }
 ]
 
 import { motion } from "framer-motion";
@@ -30,13 +24,63 @@ import { motion } from "framer-motion";
 export default function Contact() {
   return (
     <motion.section
-      initial={{opacity:0}}
-      animate = {{
-        opacity:1,
-        transition:{delay:2.4, duration:0.4, ease:"easeIn"}
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" }
       }}
+      className="py-6"
     >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          {/* form */}
+          <div className="xl:h-[54%] order-2 xl:order-none">
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl" action="">
+              <h3 className="text-4xl text-accent">
+                Let`s work together
+              </h3>
+              <p className="text-white/60">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, quis, dicta a nesciunt ex eveniet accusantium molestias nemo consequuntur consequatur maxime omnis, sed quas. Molestias voluptates vitae harum possimus rem?
+              </p>
+              {/* input */}
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <Input type="firstname" placeholder="Firstname" />
+                <Input type="lastname" placeholder="Lastname" />
+                <Input type="email" placeholder="Email address" />
+                <Input type="phone" placeholder="Phone number" />
+              </div>
+              {/* textarea */}
+              <Textarea
+                className="h-[200px]"
+                placeholder="Type your message here."
+              />
+              {/* button */}
+              <Button className="max-w-40">
+                Send Message
+              </Button>
+            </form>
+          </div>
 
+          {/* info */}
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => {
+                return (
+                  <li key={index} className="flex items-center gap-6">
+                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                      <div className="text-[28px]">{item.icon}</div>
+                    </div>
+                    <div className="flex-1">
+                      <p className="text-white/60">{item.title}</p>
+                      <h3 className="text-xl">{item.description}</h3>
+                    </div>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
 
     </motion.section>
   );
