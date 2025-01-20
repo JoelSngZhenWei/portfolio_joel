6fcaee8f404df61220c370110ebd2ee9fa5b7a59
JoelSngZhenWei <joelsngzw@gmail.com>
2025-01-20 11:04:46 +0800

    testing vercel deployment

--- portfolio_joel_1/app/page.tsx
+++ portfolio_joel_1/app/page.tsx
@@ -8,16 +8,16 @@ export default function Home() {
   return (
     <section className="h-full xl:px-16">
       <div className="container mx-auto h-full mb-4">
-        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-9 xl:pb-24">
+        <div className="flex flex-col lg:flex-row items-center justify-center lg:gap-20 lg:pt-9 lg:pb-24">
           {/* text */}
-          <div className="text-center xl:text-left order-2 xl:order-none">
+          <div className="text-center lg:text-left order-2 lg:order-none">
             <span className="text-xl font-bold">
               Data Scientist
             </span>
-            <h1 className="h2 xl:h1 mb-4 xl:mb-6">
+            <h1 className="h2 lg:h1 mb-4 lg:mb-6">
               <span className="text-accent">Joel Sng</span>
             </h1>
-            <p className="max-w-[500px] mb-4 xl:mb-9 text-white/80">
+            <p className="max-w-[500px] mb-4 lg:mb-9 text-white/80">
               I am a first class honours double degree undergraduate specialising in machine learning, automation, and software development.
             </p>
             {/* btn and socials */}
@@ -28,14 +28,14 @@ export default function Home() {
                   <FiDownload className="text-xl" />
                 </a>
               </Button>
-              <div className="xl:mb-0">
+              <div className="lg:mb-0">
                 <Socials containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
               </div>
             </div>
           </div>
 
           {/* Photo */}
-          <div className="order-1 xl:order-none mb-5 xl:mb-0">
+          <div className="order-1 lg:order-none mb-5 lg:mb-0">
             <Photo />
           </div>
         </div>
